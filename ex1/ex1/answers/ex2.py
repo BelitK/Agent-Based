36da3a6cc27620718422ae1632567b06b8d53f2f
B2K <belitberdelk@gmail.com>
2025-11-03 21:49:42 +0100

    forgot to update ex2

--- ex1/ex1/answers/ex2.py
+++ ex1/ex1/answers/ex2.py
@@ -2,22 +2,46 @@
 import mango
 import time
 import asyncio
-from Test_Agent import PVAgent
+
+class PVAgent(mango.Agent):
+    def __init__(self, neighbor=None):
+        super().__init__()
+        self.message_count = 0
+        self.neighbor = neighbor
+        print("Hello I am a Reflexive agent!")
+
+    def handle_message(self, content, meta):
+        # increment counter and show receipt
+  
+        print(f"[{self.addr}] Received ({self.message_count}): {content}  meta={meta}")
+        print(f"message count is {self.message_count}")
+        if self.message_count < 10:
+            self.message_count += 1
+            self.schedule_instant_message("Ping" if str(content).lower().startswith("pong") else "Pong", mango.sender_addr(meta))
+        
+
+
+    def get_message_count(self):
+        return self.message_count
 
 container = mango.create_tcp_container(('127.0.0.1', 5555))
-print_agent = mango.PrintingAgent()
 Aagent = PVAgent()
-Bagent = PVAgent(print_agent)
+Bagent = PVAgent()
+
+# Register neighbor
+Aagent.neighbor = Bagent
+Bagent.neighbor = Aagent
 
 # Register agent
 container.register(Aagent)
-container.register(print_agent)
-container.register(Bagent) # neighbor is the printing agent
+container.register(Bagent)
 
 async def agent_test():
     async with mango.activate(container):
-        # Kick off the ping-pong: A -> B, include sender in meta
-        Aagent.schedule_instant_message("Ping", Bagent.addr)
+        # Kick off the ping-pong: A -> B, include sender in meta, initial message from container to A
+        # starter wont count towards the 10 messages
+        Aagent.schedule_instant_message("Ping", Aagent.neighbor.addr)
+
 
         # wait until either agent reaches 10 messages
         while max(Aagent.get_message_count(), Bagent.get_message_count()) < 10:
